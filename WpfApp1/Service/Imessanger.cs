ba2653e0ee7a94e7676e122f59468110fdb9582e
goodgood619 <gktgnjftm@naver.com>
2019-08-01 22:06:22 +0900

    재업로드중

--- WpfApp1/Service/Imessanger.cs
+++ WpfApp1/Service/Imessanger.cs
@@ -5,10 +5,13 @@ using System.Text;
 using System.Threading.Tasks;
 using WpfApp1.Models;
 using WpfApp1.Modules;
+using System.Collections.ObjectModel;
 namespace WpfApp1.Service
 {
     public interface Imessanger
     {
         MessengerClient GetMessenger(Action<TCPmessage> action);
+        ObservableCollection<Frienddata> frienddatas();
+
     }
 }
