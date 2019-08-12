9efff73a47f1a37732606c8f8a1272d6dec69b6c
goodgood619 <gktgnjftm@naver.com>
2019-08-12 20:14:23 +0900

    채팅방을 나갔습니다가 너무 여러번 뜸, 그리고 X버튼 눌렀을때 나간것처럼 처리가 되어야 하는데 아직 안됨

--- WpfApp1/ViewModel/JoinChatViewModel.cs
+++ WpfApp1/ViewModel/JoinChatViewModel.cs
@@ -55,32 +55,37 @@ namespace WpfApp1.ViewModel
             switch (check)
             {
                 
-                case 1:
-                    MessageBox.Show("방에 초대되었습니다.");
-                    ChatViewModel chatViewModel = new ChatViewModel(_imessanger);
-                    chatViewModel.Chatnumber =Chatnumber;
-                    chatViewModel.NICKNAME = message;
-                    App.Current.Dispatcher.InvokeAsync(() =>
-                    {
-                        ChatView chatView = new ChatView(chatViewModel);
-                        chatView.Show();
-                    });
-
-                    break;
+               
             }
 
         }
         public void Validjoinchat(int check, string message, int Chatnumber)
         {
-            // 초대할 친구 아이디가 없거나, 로그아웃 했거나, 차단을 했거나를 구현해야함
+            // 채팅하면서 이제 중간에 계속 로그아웃을 했는지를 체크를 해줘야함
             switch (check)
             {
                 case 0:
-                    MessageBox.Show("초대할 친구가 차단하여 초대가 불가능합니다.");
+                    MessageBox.Show("초대할 아이디가 로그아웃된 상태입니다.");
                     JoinchatId = string.Empty;
                     break;
                 case 1:
-                    MessageBox.Show("친구가 초대되었습니다.");
+                    //MessageBox.Show("친구가 초대되었습니다.");
+                    JoinchatId = string.Empty;
+                    break;
+                case 2: //오류
+                    MessageBox.Show("초대할 아이디가 존재하지 않습니다. 다시입력해주세요");
+                    JoinchatId = string.Empty;
+                    break;
+                case 3:
+                    MessageBox.Show("초대하려는 아이디와 초대할 아이디가 일치합니다. 다시입력해주세요");
+                    JoinchatId = string.Empty;
+                    break;
+                case 4:
+                    MessageBox.Show("이미 이 방에 초대가 되어있는 상태입니다. 다시 입력해주세요");
+                    JoinchatId = string.Empty;
+                    break;
+                case 5: // 왜 2번뜨지
+                    MessageBox.Show("초대할 아이디를 친구차단했거나, 초대할 아이디인 사람이 친구차단을 했습니다.");
                     JoinchatId = string.Empty;
                     break;
             }
